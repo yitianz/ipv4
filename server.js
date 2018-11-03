6d7d08a15541123cd99c51e73ff87bd21988f4b4
joindaclubb <raymondclubb@berkeley.edu>
2018-11-03 15:39:33 -0700

    accesses mongodb

--- server.js
+++ server.js
@@ -5,6 +5,8 @@
 var express        = require('express');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
+var mongoose       = require('mongoose');
+var console        = require('console');
 
 var app = express();
 
@@ -18,7 +20,15 @@ var port = process.env.PORT || 8080;
 
 // connect to our mongoDB database 
 // TODO: (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url); 
+// mongoose.connect(db.url);
+
+mongoose.connect("mongodb+srv://ray:raypassword@cluster0-sv1an.mongodb.net/admin").then(
+    () => { console.log("SUCCESS: Connected to MongoDB Atlas") /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+    err => { console.log("FAIL: Failed to connect to MongoDB Atlas"); process.exit(-1) /** handle initial connection error */ }
+);;
+
+var database = mongoose.connection;
+
 
 // get all data/stuff of the body (POST) parameters
 // parse application/json 
